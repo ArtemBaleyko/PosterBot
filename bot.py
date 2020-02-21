4fbc2fdfd0b941c5daad3d3b90e7599da98b6737
Artem Baleyko <artembaleyko@gmail.com>
2020-02-21 11:41:08 +0200

    Create new bot

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
-print("Privit")
+import telebot
+import config 
+
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(commands = ['start'])
+def privit(message):
+    bot.send_message(message.chat.id, "Privit")
+
+# RUN
+bot.polling(none_stop=True)
